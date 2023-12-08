f87f2584ce0e6d8217ebfb7e8c55626fe8320e1e
33-66 <simon.nganga@student.moringaschool.com>
2023-12-08 09:14:32 +0300

    functions done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    greet = print("Hello, programmer!")
+    return greet
+    
+greet_programmer()
 
 def greet(name):
-    pass
+    greeting = print(f"Hello, {name}!")
+    return greeting
+    
+greet("simon")    
 
 def greet_with_default(name="programmer"):
-    pass
+    default_greetings = print(f"Hello, {name}!")
+    return default_greetings
+    
+greet_with_default()    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(45,55)
 
 def halve(number):
-    pass
+    return number/2
+
+halve(20) 
